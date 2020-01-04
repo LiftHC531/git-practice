cea5fc8ca4ca6c03c01415b082b94608a9e7df28
LiHsin Chen <jack661260@gmail.com>
2020-01-04 10:38:33 +0800

    command test

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#git config -l
+#git remote show origin
+#git branch -r ##show remote branch
+###git fetch origin remote_branch_name:local_branch_name
+
+git add --a
+git status
+git commit
+git status
+git push
+
